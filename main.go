df292949e93ef1445064b2bdd91902a4f15c8bc7
baysideengineer <sascha@bayside.engineer>
2024-07-06 15:47:51 +0200

    feat: add main file

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+  "fmt"
+
+  "silversteinsoftware/tour-ics-file-generator/pkg/eventDateParser"
+  "silversteinsoftware/tour-ics-file-generator/pkg/fileHandler"
+  "silversteinsoftware/tour-ics-file-generator/pkg/icsEventParser"
+  "silversteinsoftware/tour-ics-file-generator/pkg/icsTemplate"
+  "silversteinsoftware/tour-ics-file-generator/pkg/types"
+)
+
 func main() {
-  return
+  tourDates := fileHandler.ReadFile[[]types.TourEntry]("dates.json")
+  timezones := fileHandler.ReadFile[[]types.Timezone]("timezones.json")
+
+  var icsEvent string
+
+  for _, tourDate := range tourDates {
+    utcTourDate := eventDateParser.ParseTourDate(tourDate, timezones)
+    icsEvent += icsEventParser.CreateICSEvent(tourDate, utcTourDate)
+  }
+
+  wrappedICSEvent := fmt.Sprintf(icsTemplate.ICSWrapper, icsEvent)
+  fileHandler.WriteFile("events.ics", wrappedICSEvent)
 }
