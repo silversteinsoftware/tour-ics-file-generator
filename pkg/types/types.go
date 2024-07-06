bde726b6ae42e9b84d694bf29f5e26c1af603115
baysideengineer <sascha@bayside.engineer>
2024-07-06 15:52:24 +0200

    feat: add initial types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,10 +1,28 @@
 package types
 
-type InputEvent struct {
+type TourEntry struct {
   ID         int    `json:"id"`
   ShowNumber int    `json:"showNumber"`
   Date       string `json:"date"`
   Time       string `json:"time"`
   Location   string `json:"location"`
   Venue      string `json:"venue"`
+}
+
+type Timezone struct {
+  ID             int    `json:"id"`
+  TzAbbreviation string `json:"tz_abbreviation"`
+  TzName         string `json:"tz_name"`
+  TzIdentifier   string `json:"tz_identifier"`
+  UTCOffset      string `json:"utc_offset"`
+}
+
+type ICSEvent struct {
+  UID         string
+  Summary     string
+  Location    string
+  Description string
+  DtStamp     string
+  DtStart     string
+  DtEnd       string
 }
